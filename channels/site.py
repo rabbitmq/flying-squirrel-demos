13402d0d753d9e56d2a3a0907773a5c8c20655fd
Michael Bridgen <mikeb@rabbitmq.com>
2011-05-06 18:28:48 +0100

    Initial commit: channels demo

---
+++ channels/site.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import web
+import flyingsquirrel.client as squirrel
+
+from config import SERVICE_URL, SQUIRREL_SITE_URL
+
+urls = (
+    '/(index(.html)?)?', 'Channels'
+)
+app = web.application(urls, globals())
+
+render = web.template.render('.')
+
+class Channels:
+    def GET(self, *_ignore):
+        (conn_url, ticket) = create_endpoint()
+        return render.index(SQUIRREL_SITE_URL, conn_url, ticket)
+
+def create_endpoint():
+    client = squirrel.Client(SERVICE_URL)
+    endpoint = client.create_endpoint(
+        'fixme',
+        definition={'pub': ['pub', 'fixme_pubsub'],
+                    'sub': ['sub', 'fixme_pubsub'],
+                    'push': ['push', 'fixme_pushpull'],
+                    'pull': ['pull', 'fixme_pushpull'],
+                    'req': ['req', 'fixme_reqrep'],
+                    'rep': ['rep', 'fixme_reqrep']})
+    ticket = client.generate_ticket('fixme', 'fixme')
+    return endpoint['protocols']['socket.io'], ticket
+
+if __name__ == "__main__":
+    app.run()
