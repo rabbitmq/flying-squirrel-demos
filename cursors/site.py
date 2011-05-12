08d5cea609f072682e1bb3a0e780a2023c99c92c
Marek Majkowski <marek@rabbitmq.com>
2011-05-12 14:12:07 +0100

    Imported flying-cursors demo

---
+++ cursors/site.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import random
+import string
+import web
+import flyingsquirrel.client as squirrel
+
+from config import SQUIRREL_SITE_URL
+
+
+urls = (
+    '/(index(.html)?)?', 'Cursors'
+)
+app = web.application(urls, globals())
+
+render = web.template.render('.')
+
+class Cursors:
+    def GET(self, *_ignore):
+        username, transport_url, ticket = create_endpoint()
+        return render.index(username, transport_url, ticket)
+
+def create_endpoint():
+    client = squirrel.Client(SQUIRREL_SITE_URL)
+    endpoint = client.create_endpoint(
+        "flying_cursors",
+        definition = {'egress': ['pub', 'flying_cursors'],
+                      'ingress': ['sub', 'flying_cursors']})
+    username = ''.join(random.choice(string.letters) for i in xrange(8))
+    ticket = client.generate_ticket('flying_cursors', username)
+    return (username, endpoint['protocols']['socket.io'], ticket)
+
+if __name__ == "__main__":
+    app.run()
