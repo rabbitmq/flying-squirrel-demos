94b141ce922f3054e53f17ffd7c0617e748c0f60
Michael Bridgen <mikeb@rabbitmq.com>
2011-06-07 16:32:53 +0100

    Use a random name for each endpoint so messages don't go astray

--- channels/site.py
+++ channels/site.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import web
+import random
+import string
 import flyingsquirrel as squirrel
 
 from config import SERVICE_URL, SQUIRREL_SITE_URL
@@ -19,15 +21,16 @@ class Channels:
 
 def create_endpoint():
     client = squirrel.API(SERVICE_URL)
+    name = ''.join([random.choice(string.letters) for i in range(10)])
     endpoint = client.create_endpoint(
-        'fixme',
+        name,
         definition={'pub': ['pub', 'fixme_pubsub'],
                     'sub': ['sub', 'fixme_pubsub'],
                     'push': ['push', 'fixme_pushpull'],
                     'pull': ['pull', 'fixme_pushpull'],
                     'req': ['req', 'fixme_reqrep'],
                     'rep': ['rep', 'fixme_reqrep']})
-    ticket = client.generate_ticket('fixme', 'fixme')
+    ticket = client.generate_ticket(name, 'anon')
     return endpoint['protocols']['socket.io'], ticket
 
 if __name__ == "__main__":
