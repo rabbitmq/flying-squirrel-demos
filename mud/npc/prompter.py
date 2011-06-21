df0179e8052274d6cc73c57901fc931b97d70f6f
Michael Bridgen <mikeb@rabbitmq.com>
2011-06-21 16:46:50 +0100

    Use the argument supplied rather than looking prompt up by name

--- mud/npc/prompter.py
+++ mud/npc/prompter.py
@@ -10,22 +10,17 @@ lines = render_to_string('_lines.txt', {}).split('\n')
 def probability(n):
     return bool(random.randrange(0, 100) < n)
 
-def prompter():
-    return models.Char.objects.get(nick__exact="Prompt")
-
 def got_tell(actor=None, target=None, text='', **kwargs):
     if text == 'line' or text == 'line?':
         line = random.choice(lines)
         commands._do_tell(target, actor, '"%s"' % (line,))
     else:
-        actor.send("%s arches his eyebrow expectantly" % (prompter(),))
+        actor.send("%s arches his eyebrow expectantly" % (target,))
 
-def got_someone_said(actor=None, **kwargs):
-    me = prompter()
-    if actor != me:
-        commands._do_say(me, "That's not the line! Get off the stage!" )
+def got_someone_said(actor=None, target=None, **kwargs):
+    if actor != target:
+        commands._do_say(target, "That's not the line! Get off the stage!" )
         if probability(20):
-            actor.send("You feel a hard kick from %s on your back." %(me,))
-            actor.send_to_others("%s kicks %s off the stage." % (me, actor))
+            actor.send("You feel a hard kick from %s on your back." %(target,))
+            actor.send_to_others("%s kicks %s off the stage." % (target, actor))
             commands.do_go(actor,  'e')
-
